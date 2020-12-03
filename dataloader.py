ff97a0ff0df0bba3b3072a8d433d0d916dab4274
nakuram <minorunakura@gmail.com>
2020-12-03 01:26:44 -0800

    added grayscale option, also bug in dataloader but we can work WITH the bug because idk how to fix it

--- dataloader.py
+++ dataloader.py
@@ -5,7 +5,8 @@ import numpy as np
 import cv2 
 from PIL import Image 
 
-""" A lot of credit goes to Chris Xie for his code on dataloader stuff.
+""" AUXILIARY FUNCTIONS
+    A lot of credit goes to Chris Xie for his code on dataloader stuff.
 """
 
 def standardize_images(image):
@@ -28,14 +29,19 @@ def image_to_tensor(image):
         tensor = torch.from_numpy(image).permute(2,0,1).float()
     return tensor
 
-class AnimeDataset(Dataset):
-    """ dataloader for anime images
+
+""" DATASETS AND DATALOADERS
+"""
+
+class PhotoDataset(Dataset):
+    """ dataloader for photo images only
     """
 
-    def __init__(self, base_dir):
+    def __init__(self, base_dir, grayscale=False):
         self.base_dir = base_dir
         self.all_images = os.listdir(base_dir)
         self.len = len(self.all_images)
+        self.grayscale = 0 if grayscale else 1
 
     def __len__(self):
         return self.len
@@ -43,12 +49,112 @@ class AnimeDataset(Dataset):
     def __getitem__(self, idx):
         image_path = self.base_dir + self.all_images[idx]
         # print(image_path)
-        image = cv2.imread(image_path, 1)
+        image = cv2.imread(image_path, self.grayscale)
         image = standardize_images(image)
         image = image_to_tensor(image)
         return image
         
-def getThreeShapesDataloader(base_dir, batch_size=4, shuffle=True):
+def getPhotoDataloader(base_dir, batch_size=4, shuffle=True):
+    dataset = PhotoDataset(base_dir)
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
+
+# path = './dataset/train_photo/'
+# ad = PhotoDataset(path)
+# im = ad[1]
+# print(im.shape)
+
+# dl = getPhotoDataloader(path)
+# batch = next(iter(dl))
+# print(batch.shape)
+
+class PhotoAndAnimeDataset(Dataset):
+    """ dataloader for photo and anime images
+    """
+
+    def __init__(self, photo_base_dir, anime_base_dir, grayscale=False):
+        # first half is anime images
+        self.anime_base_dir = anime_base_dir
+        self.anime_images = os.listdir(anime_base_dir)
+        self.num_anime_photos = len(self.anime_images)
+
+        # second half is legit images
+        self.photo_base_dir = photo_base_dir
+        self.photo_images = os.listdir(photo_base_dir)
+
+        # append all together
+        self.all_images = self.anime_images.copy()
+        self.all_images.extend(self.photo_images)
+        self.len = len(self.all_images)
+        self.grayscale = 0 if grayscale else 1
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        """ 1s for anime photos and 0s for real photos
+            BUG: when using dataloader, the 1s and 0s are flipped, which 
+            makes no sense since it works when using getitem directly.
+            
+            TEMPORARY FIX: just roll with the bug mann. I return the opposite
+            values.
+
+        """
+        label = None
+        if idx < self.num_anime_photos:
+            image_path = self.anime_base_dir + self.all_images[idx]
+            label = torch.zeros(1)
+        else:
+            image_path = self.photo_base_dir + self.all_images[idx]
+            label = torch.ones(1)
+
+        # print(image_path)
+        image = cv2.imread(image_path, self.grayscale)
+        image = standardize_images(image)
+        image = image_to_tensor(image)
+        
+        return image, label
+        
+def getPhotoAndAnimeDataloader(photo_base_dir,anime_base_dir, batch_size=4, shuffle=True):
+    dataset = PhotoAndAnimeDataset(photo_base_dir, anime_base_dir)
+    return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
+
+# anime_path = './dataset/Shinkai/smooth/'
+# photo_path = './dataset/train_photo/'
+
+# paad = PhotoAndAnimeDataset(photo_path, anime_path)
+# im, label = paad[2000]
+# print(im.shape)
+# print(label)
+
+# dl = getPhotoAndAnimeDataloader(anime_path, photo_path, batch_size=4)
+# images, labels = next(iter(dl))
+# print(images.shape)
+# print(labels)
+
+
+class AnimeDataset(Dataset):
+    """ dataloader for anime images
+        Am I dumb? Is this not needed at all??
+    """
+
+    def __init__(self, base_dir, grayscale=False):
+        self.base_dir = base_dir
+        self.all_images = os.listdir(base_dir)
+        self.len = len(self.all_images)
+        self.grayscale = 0 if grayscale else 1
+
+    def __len__(self):
+        return self.len
+
+    def __getitem__(self, idx):
+        image_path = self.base_dir + self.all_images[idx]
+        # print(image_path)
+        image = cv2.imread(image_path, self.grayscale)
+        image = standardize_images(image)
+        image = image_to_tensor(image)
+        return image
+        
+def AnimeDataloader(base_dir, batch_size=4, shuffle=True):
     dataset = AnimeDataset(base_dir)
     return DataLoader(dataset=dataset, batch_size=batch_size, shuffle=shuffle)
 
@@ -57,6 +163,6 @@ def getThreeShapesDataloader(base_dir, batch_size=4, shuffle=True):
 # im = ad[1]
 # print(im.shape)
 
-# dl = getThreeShapesDataloader(path)
+# dl = getAnimeDataloader(path)
 # batch = next(iter(dl))
 # print(batch.shape)
